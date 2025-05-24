bfc6fe1b618edcb531de967cbeda76babfbf7d59
ronymia <rony.mia7800@gmail.com>
2025-05-24 14:40:40 +0600

    feat: implement Create Department page with form and submission handling

--- src/app/(withLayout)/super_admin/department/page.tsx
+++ src/app/(withLayout)/super_admin/department/page.tsx
@@ -1,5 +1,49 @@
+import ActionBar from "@/components/ui/ActionBar";
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { Button } from "antd";
+import Link from "next/link";
+
 const Department = () => {
-  return <div>Department</div>;
+  return (
+    <div>
+      <UMBreadCrumb
+        items={[
+          {
+            label: "super_admin",
+            link: "/super_admin",
+          },
+        ]}
+      />
+
+      <ActionBar title="Department List">
+        {/* <Input
+          type="text"
+          size="large"
+          placeholder="Search..."
+          style={{
+            width: "20%",
+          }}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        /> */}
+        <div>
+          <Link href="/super_admin/department/create">
+            <Button type="primary">Create</Button>
+          </Link>
+          {/* {(!!sortBy || !!sortOrder || !!searchTerm) && (
+            <Button
+              onClick={resetFilters}
+              type="primary"
+              style={{ margin: "0px 5px" }}
+            >
+              <ReloadOutlined />
+            </Button>
+          )} */}
+        </div>
+      </ActionBar>
+    </div>
+  );
 };
 
 export default Department;
