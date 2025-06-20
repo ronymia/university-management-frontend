17bfb92c300409626da7ec829617a36435702791
MD Rony Mia <rony.mia7800@gmail.com>
2025-06-20 12:23:04 +0600

    refactor: Simplify OfferedCourseSectionForm usage by removing unnecessary router logic in create and edit pages

--- src/app/(withLayout)/admin/offered-course-section/create/page.tsx
+++ src/app/(withLayout)/admin/offered-course-section/create/page.tsx
@@ -1,11 +1,5 @@
-"use client";
-
 import OfferedCourseSectionForm from "../OfferedCourseSectionForm";
-import { useRouter } from "next/navigation";
 
 export default function CreateOfferedCourseSectionPage() {
-  const router = useRouter();
-  return (
-    <OfferedCourseSectionForm id={""} popupCloseHandler={() => router.back()} />
-  );
+  return <OfferedCourseSectionForm id={""} />;
 }
--- src/app/(withLayout)/admin/offered-course-section/edit/[id]/page.tsx
+++ src/app/(withLayout)/admin/offered-course-section/edit/[id]/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import React, { use } from "react";
+import { use } from "react";
 import OfferedCourseSectionForm from "../../OfferedCourseSectionForm";
-import { useRouter } from "next/navigation";
 
 type IDProps = {
   params: Promise<{
@@ -14,8 +13,5 @@ export default function UpdateOfferedCourseSectionPage({ params }: IDProps) {
   const resolvedParams = use(params);
   const { id } = resolvedParams;
 
-  const router = useRouter();
-  return (
-    <OfferedCourseSectionForm id={id} popupCloseHandler={() => router.back()} />
-  );
+  return <OfferedCourseSectionForm id={id} />;
 }
