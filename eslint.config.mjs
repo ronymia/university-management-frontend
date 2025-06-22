6f97b52980f9c3dcab3c5738b14d481520a557bd
MD Rony Mia <rony.mia7800@gmail.com>
2025-06-22 12:37:19 +0600

    feat: add @typescript-eslint/eslint-plugin for improved TypeScript linting support refactor: update eslint configuration for better readability and consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,38 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "dist/**",
-      "build/**",
-      "coverage/**",
-      "prettier.config.mjs",
-    ],
-  },
-  // ESLint rules from Next.js & TS
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // Disable ESLint rules that conflict with Prettier
-  ...compat.extends("prettier"),
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+    {
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'dist/**',
+            'build/**',
+            'coverage/**',
+            'prettier.config.mjs',
+        ],
     },
-  },
+    // ESLint rules from Next.js & TS
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+    // Disable ESLint rules that conflict with Prettier
+    ...compat.extends('prettier'),
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/consistent-type-assertions': 'error',
+            '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+        },
+    },
 ];
 
 export default eslintConfig;
