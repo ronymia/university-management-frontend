75389abd2eacf6bd7f6b993c767575ae69f6ef74
MD Rony Mia <rony.mia7800@gmail.com>
2025-07-07 11:45:56 +0600

    Add Profile

--- src/app/(withLayout)/profile/page.tsx
+++ src/app/(withLayout)/profile/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import UserProfile from '@/components/ui/Common/User/UserProfile';
+import { useAppSelector } from '@/redux/hooks';
 
-const ProfilePage = () => {
-    return <div>Under development</div>;
-};
-
-export default ProfilePage;
+export default function ProfilePage() {
+    const { user } = useAppSelector((state) => state.auth);
+    return <UserProfile userId={user?.id} />;
+}
